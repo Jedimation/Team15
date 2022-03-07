bbc8544f3a5eebc4b1b448fa6c3867fedc831208
Jedimation <98482011+Jedimation@users.noreply.github.com>
2022-03-07 02:30:17 +0200

    Yes Great work
    Saved

--- Travelheader.php
+++ Travelheader.php
@@ -18,7 +18,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <div class="navbar-header">
-        <a class="img-fluid navbar-brand" href="#"><img class="logo" src= "C:\PHPCourse\src\Team15\vacation-logos.jpg" alt="Experince Life" width="150"></a> 
+        <a class="img-fluid navbar-brand" href="#"><img class="logo" src= "vacation-logos.jpg" alt="Experince Life" width="150"></a> 
         
     </div>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
---
+++ Travelhome.php
@@ -0,0 +1,14 @@
+<?php
+$title = " WOW";
+$md = "Go";
+include 'header.php' ;?>
+
+<h1> Adewale</h1>
+<p> This is a new day </p>
+
+
+
+
+<?php
+
+include 'footer.php' ;?>
